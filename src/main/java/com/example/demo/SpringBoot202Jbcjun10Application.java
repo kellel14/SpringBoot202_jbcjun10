6df97bcf59aac4eb92a0dcd5bcd2094b4b1224cd
kellel14 <kellel14@gmail.com>
2019-07-15 11:02:22 -0400

    first commit

---
+++ src/main/java/com/example/demo/SpringBoot202Jbcjun10Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot202Jbcjun10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot202Jbcjun10Application.class, args);
+	}
+
+}
